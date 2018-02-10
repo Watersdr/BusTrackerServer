b2f7308b937603edf06a375c7a0a97e5d1cd2830
Orry Jarvis <oajarvis@Ike.local>
2018-02-10 14:57:28 -0500

    primitive code to send random latitude/longitude coords to client

--- server.js
+++ server.js
@@ -4,7 +4,37 @@ const wss = new WebSocket.Server({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
   console.log('hey man');
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+  // Maximimum latitude and longitude values (min = -max)
+  const maxLat = 85;
+  const maxLong = 180;
+
+  // Calculate a minute of latitude/longitude
+  // = 1/(range * 60)
+  const minuteLat = 1/(maxLat*120);
+  const minuteLong = 1/(maxLong*120);
+
+  // Initialize lat and long anywhere between max/min bounds
+  let lat = Math.random() * maxLat * (Math.random < 0.5 ? -1 : 1);
+  let long = Math.random() * maxLong * (Math.random < 0.5 ? -1 : 1)
+  
+  while(1) {
+    
+        // Send to Client
+      setTimeout(() => {
+          ws.send({latitude: lat.toString(), longitude: long.toString()});
+      }, 5000);
+
+      //Update latitude and longitude up to a minute increment
+      lat += Math.random() * minuteLat * (Math.random < 0.5 ? -1 : 1);
+      long += Math.random() * minuteLong * (Math.random < 0.5 ? -1 : 1);
+
+      // Bounds check
+      lat = (lat > maxLat ? maxLat : lat);
+      lat = (lat < -maxLat ? -maxLat : lat);
+      long = (long > maxLong ? maxLong : long);
+      long = (long < -maxLong ? -maxLong : long);
+  }
+  //ws.on('message', function incoming(message) {
+     //console.log('received: %s', message);
+  //});
 });
